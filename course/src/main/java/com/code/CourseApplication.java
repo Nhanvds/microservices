eb4570fcc977ab0baec99cd29f45546f6247b989
Nhanvd <nhanvu232003@gmail.com>
2025-04-16 14:23:43 +0700

    change file properties

--- course/src/main/java/com/code/CourseApplication.java
+++ course/src/main/java/com/code/CourseApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/*
- * @PropertySources({
- * 
- * @PropertySource({"file:C:\\Users\\Admin\\microservice\\course.properties"})
- * })
- */
 public class CourseApplication {
 
 	public static void main(String[] args) {
